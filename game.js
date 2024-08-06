924b10880a8f83813fed2ca07b254afc66217893
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-08-06 16:13:02 +0400

    Add files via upload

--- game.js
+++ game.js
@@ -61,15 +61,7 @@ function upgrade() {
     
 }
 
-function buy_ferm() {
-    var farm = window.localStorage.getItem('farm');
-    if (farm == null) {
-        window.localStorage.setItem('farm', 1)
-    }
-    else {
-        window.localStorage.setItem('farm', 1 + farm)
-    }
-}
+
 
 
 
